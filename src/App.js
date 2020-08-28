7e6ab7fc3861164993e3ad3f96d55d94c0ee5cbb
ShaheenHuzir <40265125+ShaheenHuzir@users.noreply.github.com>
2020-08-28 12:20:47 +0300

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Toggler from './components/toggle.js';
+
 import './App.css';
+import classNames from 'classnames';
 
 function App() {
+  let [mode, setMode] = useState(false);
+
+  let modeHandler = (toggledMode) => {
+    setMode(toggledMode);
+  };
+
+  let boom = classNames({ hello: !mode, yello: mode });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={('container', boom)}>
+      <Toggler modeChange={modeHandler} />
     </div>
   );
 }
---
+++ src/components/toggle.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+import './toggle.css';
+
+function Toggler(props) {
+  let { modeChange } = props;
+  let toggleValue;
+  useEffect(() => {
+    let checker = document.getElementById('checker');
+    toggleValue = () => {
+      modeChange(checker.checked);
+    };
+  });
+
+  return (
+    <div>
+      <input
+        type='checkbox'
+        id='checker'
+        class='switch'
+        onClick={() => toggleValue()}
+
+        //doubt 1 : why is it supposed to be a callback function???
+      />
+      <label for='checker' class='btn-toggle'></label>
+    </div>
+  );
+}
+
+export default Toggler;
